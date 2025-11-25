1759e0852f6789991e387d15de5d47668f048a6e
Danny Palacios <dannyux@gmail.com>
2025-11-25 15:15:59 +0000

    result

--- app.py
+++ app.py
@@ -10,6 +10,10 @@ import os
 
 app = Flask(__name__)
 
+@app.route('/')
+def index():
+    return render_template('index.html', result=None)
+
 # Configuración
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'clave-secreta')
 
